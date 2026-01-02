530e9617a4a1c7b46fc09c77768b916c84ed6d77
rohanbala-100 <rb1090496@gmail.com>
2026-01-02 19:29:03 +0530

    Update spam_classifier.py
    Data loading and label preprocessing

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,1 +1,14 @@
+#Day 2
+import pandas as pd
 
+print("Program started")
+
+data = pd.read_csv("data/spam.csv", encoding="latin-1")
+
+data = data[['v1', 'v2']]
+data.columns = ['label', 'message']
+
+data['label'] = data['label'].map({'ham': 0, 'spam': 1})
+
+print(data.head())
+print("Rows and columns:", data.shape)
